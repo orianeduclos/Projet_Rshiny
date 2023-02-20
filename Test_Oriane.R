9e2d051979007e6c8dec55b9f013d47df380f1eb
orianeduclos <oriane.dcls@gmail.com>
2023-02-20 15:44:57 +0100

    ajout Rmarkdown

--- Test_Oriane.R
+++ Test_Oriane.R
@@ -1,17 +1,11 @@
+
+##### Base de donnée etat civil ####
 etat_civil <- read.csv("data/FD_NAIS_2021.csv", header= TRUE, sep=';')
 head(etat_civil)
 
-bebe <- read.table("data/bebe.txt", header = TRUE, sep = ";")
-head(bebe)
-dim(bebe)
-
-taux_fecondite <- read.csv("data/taux_fecondite.csv", header= TRUE, sep=',')
-head(taux_fecondite)
-View(taux_fecondite)
 
 
-
-#### Boxplot Age mere age pere ####
+###### Boxplot Age mere age pere ######
 
 library(rAmCharts)
 library(tidyverse)
@@ -34,9 +28,42 @@ ggplot(age,aes(x=SEXEPARENT, y= AGEPARENT, color = SEXEPARENT))+
   scale_fill_manual(values=c('#CD5C5C','#87CEFA'))+
   theme_classic()
 
+
+###### Regression ######
+
+
+#### Base de donnée BEBE ####
+bebe <- read.table("data/bebe.txt", header = TRUE, sep = ";")
+head(bebe)
+#View(bebe)
+dim(bebe)
+summary(bebe)
+
+###### NA ######
+mean(bebe$TailleBB, na.rm=TRUE)
+manquant <- is.na(bebe)
+dim(manquant)
+sum(manquant)
+
+# reparage apr ligne 
+coordmanquant <- which(manquant, arr.ind=TRUE)
+coordmanquant[1:6,]
+# eliminer doublon 
+unique(coordmanquant[,1])
+
+bool <- apply(is.na(bebe),1,any)
+names(bool) <- NULL
+which(bool)
+
+
 ggplot(bebe, aes(x=bebe$AgedelaMere))
-  
-##### Regression #####
+
+
+#### Base de donnée taux de fécondité ####
+taux_fecondite <- read.csv("data/taux_fecondite.csv", header= TRUE, sep=',')
+head(taux_fecondite)
+View(taux_fecondite)
+
 
 
 
