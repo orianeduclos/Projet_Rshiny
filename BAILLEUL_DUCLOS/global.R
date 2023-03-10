938b87bfcd3fc4908e26cc5beb67661e6a5d4923
margauxbll <margaux.bailleul@free.fr>
2023-03-10 08:26:09 +0100

    nettoyage global

--- BAILLEUL_DUCLOS/global.R
+++ BAILLEUL_DUCLOS/global.R
@@ -24,27 +24,6 @@ library(corrplot) # affichage matrice de correlation
 library(car)
 library(rsconnect) # pour publier 
 
-if (!(require(jsonlite))) install.packages("jsonlite")
-mygeocode <- function(adresses){
-  # adresses est un vecteur contenant toutes les adresses sous forme de chaine de caracteres
-  nominatim_osm <- function(address = NULL){
-    ## details: http://wiki.openstreetmap.org/wiki/Nominatim
-    ## fonction nominatim_osm proposée par D.Kisler
-    if(suppressWarnings(is.null(address)))  return(data.frame())
-    tryCatch(
-      d <- jsonlite::fromJSON(
-        gsub('\\@addr\\@', gsub('\\s+', '\\%20', address),
-             'http://nominatim.openstreetmap.org/search/@addr@?format=json&addressdetails=0&limit=1')
-      ), error = function(c) return(data.frame())
-    )
-    if(length(d) == 0) return(data.frame())
-    return(c(as.numeric(d$lon), as.numeric(d$lat)))
-  }
-  tableau <- t(sapply(adresses,nominatim_osm))
-  colnames(tableau) <- c("lon","lat")
-  return(tableau)
-}
-
 ### Base de données taux de fécondité ###
 
 taux_fecondite <- read.csv("data/taux_fecondite.csv", header= TRUE, sep=',')
@@ -54,21 +33,9 @@ levels(taux_fecondite$LOCATION) <- c("Argentine", "Australie", "Autriche", "Belg
 
 
 ### Base de donnée BEBE ###
-bebe <- read.table("data/bebe.txt", header = TRUE, sep = ";")
+bebe_ouverture <- read.table("data/bebe.txt", header = TRUE, sep = ";")
 
-## NA ##
-manquant <- is.na(bebe)
-
-# reparage par ligne 
-coordmanquant <- which(manquant, arr.ind=TRUE)
-coordmanquant[1:6,]
-# eliminer doublon 
-unique(coordmanquant[,1])
-
-bool <- apply(is.na(bebe),1,any)
-names(bool) <- NULL
-which(bool)
-bebepropre <- na.omit(bebe)
+bebepropre <- na.omit(bebe_ouverture)
 
 # transformation sexe en variable indicatrice
 bebe <- bebepropre |> 
@@ -82,31 +49,15 @@ fertility <- WDI(indicator = "SP.DYN.TFRT.IN", start = 2017, end = 2019)
 
 world <- ne_countries(scale = "medium", returnclass = "sf")
 world_fertility <- left_join(world, fertility, by = c("iso_a3" = "iso3c"))
-head(world_fertility)
-
 
 pal <- colorNumeric(palette = "YlOrRd", domain = world_fertility$SP.DYN.TFRT.IN)
 
 ### BASE DE DONNEE PRENOM 
 prenom <- read.csv("data/dpt2021.csv", header= TRUE, sep=';')
-dpt <- read_sf("data/dpt")
-prenom <- prenom |> 
-  rename("CODE_DEPT" = "dpt")
-prenom_dpt <- inner_join(prenom, dpt, by = c("CODE_DEPT"))
-
-mygeocode("France")
-France <- c(1.888334, 46.60335) 
-
-
-prenom_dpt <- aggregate(prenom_dpt$nombre, by=list(preusuel = prenom_dpt$preusuel, CODE_DEPT = prenom_dpt$CODE_DEPT), FUN=sum)
-prenom_dpt <- inner_join(prenom_dpt, dpt, by = c("CODE_DEPT"))
-prenom_dpt <- sf::st_as_sf(prenom_dpt)
 
 prenom_annees <- subset(prenom, (preusuel != "_PRENOMS_RARES") & (annais != "XXXX"))
 prenom_annees <- prenom_annees |>                                      
   group_by(preusuel, annais) |>                       
   summarise(nombre = sum(nombre))
 prenom_annees$annais <- as.Date(prenom_annees$annais, format="%Y")
-
-## WORLD CLOUD 
 
